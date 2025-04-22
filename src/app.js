5bca9ec27c2b0afd2378b39fbb795763344efabc
AbrahamIchaj <abrahamichaj95@gmail.com>
2025-04-21 21:38:24 -0600

    Install Express

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('funcionando');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor Express funcionando en http://localhost:${PORT}`);
+});
+
+exports = app;
