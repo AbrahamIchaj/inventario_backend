4b2441759a96ec0d6fb009514600f0e6b67d059a
AbrahamIchaj <abrahamichaj95@gmail.com>
2025-04-22 23:02:15 -0600

    Update Cors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import * as express from "express";
 import * as dotenv from "dotenv";
+import * as cors from "cors";
 
 dotenv.config();
 
@@ -8,13 +9,23 @@ import roleRoutes from "./routes/roleRoutes";
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// CORS
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // API'S
 app.use("/api/roles", roleRoutes);
 
-// Start the server
+// SERVER
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
